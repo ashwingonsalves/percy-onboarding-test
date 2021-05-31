7c19c4e1103a4499159199403c90d03564560108
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2021-05-31 15:05:23 +0530

    Update assignment.js
    Updated Staging links

--- test/specs/assignment.js
+++ test/specs/assignment.js
@@ -2,20 +2,20 @@ const percySnapshot = require('@percy/webdriverio');
 
 describe('BrowserStack Production', () => {
     it('Verifying popular endpoints', async () => {
-        await browser.url('https://www.browserstack.com/');
+        await browser.url('https://k8s.bsstag.com/');
         await(await $('#accept-cookie-notification')).click();
         await expect(browser).toHaveTitleContaining('Cross Browser Testing Platform');
         await percySnapshot('Home Page');
 
-        await browser.url('https://www.browserstack.com/pricing');
+        await browser.url('https://k8s.bsstag.com/pricing');
         await expect(browser).toHaveTitleContaining('BrowserStack Pricing');
         await percySnapshot('Pricing Page');
 
-        await browser.url('https://www.browserstack.com/integrations/automate');
+        await browser.url('https://k8s.bsstag.com/integrations/automate');
         await expect(browser).toHaveTitleContaining('Integrations with Automate for Testing');
         await percySnapshot('Automate Integration');
 
-        await browser.url('https://www.browserstack.com/docs/');
+        await browser.url('https://k8s.bsstag.com/docs/');
         await expect(browser).toHaveTitleContaining('Developer Documentation Home');
         await percySnapshot('Documentation');
 
